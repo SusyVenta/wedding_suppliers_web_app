d46a9cc08c87c426c1dfc12274da502690d39da1
Narachii <naoya706oxford@gmail.com>
2022-07-10 20:18:21 +0900

    implment controller router

---
+++ functions/controllers/UserController.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const Views = '../views/'
+
+const getUserProfile = ((req, res) => {
+  const id = Number(req.params.userID)
+  const params = { user: { id: id, name: "Emma Stone" } }
+
+  res.render(Views + 'user_profile.ejs',  params)
+})
+
+const getUserLogin = ((req, res) => {
+  res.render(Views + 'customer_login.ejs' )
+})
+
+const getUserRegistration = ((req, res) => {
+  // TODO implement user_reg.ejs
+})
+
+
+module.exports = {
+  getUserProfile,
+  getUserLogin,
+  getUserRegistration
+}
+
---
+++ functions/routes/users.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/UserController')
+
+router.get('/:userId/profile', userController.getUserProfile)
+router.get('/login', userController.getUserLogin)
+
+module.exports = router
