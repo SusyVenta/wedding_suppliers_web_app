081062bf2ed02207e6f5d5e79d2c567cd9b97f8c
Nathan Norton <nathangnorton@gmail.com>
2022-08-04 15:30:54 +0200

    fixed sign in

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,7 +1,6 @@
 firebase.initializeApp(config);
 const auth = firebase.auth();
 
-
 const modal = document.getElementById('modal');
 const closeModal = document.getElementById('close');
 
@@ -27,7 +26,14 @@ const createUserDialogue = document.getElementById('create-user-dialogue');
 const signInDialogue = document.getElementById('sign-in-dialogue');
 const needAccountDialogue = document.getElementById('need-account');
 
+// Get elements that need to be hidden or shown depending on signed in state
+let hidenWhenSignedIn;
+let hidenWhenSignedOut;
+
 window.addEventListener('DOMContentLoaded', () => {
+    hidenWhenSignedIn = document.querySelectorAll('.hide-when-signed-in');
+    hidenWhenSignedOut = document.querySelectorAll('.hidden-when-signed-out');
+
     document.querySelectorAll('.auth').forEach(item => {
         item.addEventListener('click', event => {
             let chosen = event.target.getAttribute('auth');
@@ -37,8 +43,10 @@ window.addEventListener('DOMContentLoaded', () => {
                 displayCreateUserForm();
             } else if (chosen === 'show-sign-in-form') {
                 displaySignInForm();
-            } else if (chose === 'show-forgot-password-form') {
+            } else if (chosen === 'show-forgot-password-form') {
                 displayForgotPassword();
+            } else if (chosen === 'sign-out') {
+                signOutUser();
             }
         })
     })
@@ -77,6 +85,11 @@ hideAuthElements = () => {
     needAccountDialogue.classList.add('hide');
 }
 
+// Called when a user signs out
+signOutUser = () => {
+    auth.signOut();
+}
+
 // User identifier (needs to be global)
 let uid;
 
@@ -86,8 +99,24 @@ auth.onAuthStateChanged(user => {
         //logged in
         uid = user.uid;
         modal.style.display = 'none';
+
+        // hide/show elements depending on if user is signed in
+        hidenWhenSignedIn.forEach(item => {
+            console.log('called');
+            item.classList.add('hide');
+        })
+
+        hidenWhenSignedOut.forEach(item => {
+            item.classList.remove('hide');
+        })
     } else {
-        //logged out
+        console.log('called');
+        hidenWhenSignedIn.forEach(item => {
+            item.classList.remove('hide');
+        })
+        hidenWhenSignedOut.forEach(item => {
+            item.classList.forEach.add('hide');
+        })
     }
 })
 
@@ -112,22 +141,27 @@ createUserForm.addEventListener('submit', event => {
 
 signInForm.addEventListener('submit', event => {
     event.preventDefault();
-    const email = document.getElementById('sign-in-email');
-    const password = document.getElementById('sign-in-password');
+    const email = document.getElementById('sign-in-email').value;
+    const password = document.getElementById('sign-in-password').value;
+    console.log(email)
+    console.log(password)
 
-    auth.signInWithEmailAndPassword(email, password)
-        .then(() => {
-            signInForm.reset();
-            hideAuthElements();
-        })
-        .catch(error => {
-            console.log(error.message);
-        })
+    if (email && password) {
+        auth.signInWithEmailAndPassword(email, password)
+            .then(() => {
+                signInForm.reset();
+                hideAuthElements();
+            })
+            .catch(error => {
+                console.log(error.message);
+            })
+    }
+
 })
 
 forgotPasswordForm.addEventListener('submit', event => {
     event.preventDefault();
-    const email = document.getElementById(forgot - password - email).value;
+    const email = document.getElementById('forgot-password-email').value;
 
     auth.sendPasswordResetEmail(email)
         .then(() => {
